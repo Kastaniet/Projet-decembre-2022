b5d332ab8f8d00935f5f7f2049a4b92e426db6bc
Kastaniet <kevin.pellissard@gmail.com>
2022-11-08 20:03:58 +0100

    main

---
+++ client/src/data.js
@@ -0,0 +1,79 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "./img/top-jeux-attendus-2022.jpg",
+        title: "TOP JEUX",
+        desc: "Les jeux les plus attendus de l'année",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "./img/sorties-jeux-video-octobre-2022.jpg",
+        title: "DERNIERES SORTIES",
+        desc: "Toutes les dernières sorties",
+        bg: 'fcf1ed',
+    },
+    {
+        id: 3,
+        img: "./img/boites-jeux-vidéo.jpg",
+        title: "RETRO GAMING",
+        desc: "Saut dans le passé avec notre section rétro",
+        bg: 'fbf0f4',
+    }
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "./img/top-games-retro.jpg",
+        title: "RETRO",
+        cat:"retro",
+    },
+    {
+        id: 2,
+        img: "./img/nouveaux-jeux-video.jpg",
+        title: "NOUVEAU",
+        cat: "nouveau",
+    },
+    {
+        id: 3,
+        img: "./img/meilleurs-jeux-video-2020.jpg",
+        title: "POPULAIRE",
+        cat:"populaire",
+    },
+];
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "./img/bayonetta-3.jpg",
+    },
+    {
+        id: 2,
+        img: "./img/elden-ring.jpg",
+    },
+    {
+        id: 3,
+        img: "./img/Sifu.jpg",
+    },
+    {
+        id: 4,
+        img: "./img/splatoon-3.jpg",
+    },
+    {
+        id: 5,
+        img: "./img/stray.jpg",
+    },
+    {
+        id: 6,
+        img: "./img/plague-tales-requiem.jpg",
+    },
+    {
+        id: 7,
+        img: "./img/horizon-forbidden-west.jpg",
+    },
+    {
+        id: 8,
+        img: "./img/Gotham-Knights.jpg",
+    }
+]
